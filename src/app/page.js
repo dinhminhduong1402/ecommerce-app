7a57726a4ed1b078cb73eb51ee19de9a1a3a808b
dinhminhduong1402 <dinhminhduong.14.02.03@gmail.com>
2023-09-03 10:35:57 +0700

    3/9/2023

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,25 @@
+import './styles/HomePage.scss'
+import { subFont } from './fonts'
 
 export default function Home() {
   return (
     <>
-      <section>HeroBanner</section>
+      <section className="hero-banner">
+        <div className="hero-banner-container">
+          <div className="hero-banner-text">
+            <h6>BOTANICAL SKINCARE</h6>
+            <h2 className={subFont.className}>
+              Skincare made with the world’s finest plant oils and absolutes.
+            </h2>
+            <p>
+              We all have skincare concerns: it is not always SIMPLE. Glowbar makes it easy with
+              30-minute expert sessions targeting your top skincare concerns.
+            </p>
+            <button>Shop now</button>
+          </div>
+        </div>
+      </section>
+
       <section>BestSeller</section>
       <section>Brands</section>
       <section>SubBanner</section>
